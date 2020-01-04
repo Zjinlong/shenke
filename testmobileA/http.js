75bcdd2221dcbccc8f2dbea4cc0807623cf3e1f7
zhangjinlong <zhangjinlong@shanju.tv>
2020-01-04 21:52:05 +0800

    zhushi

---
+++ testmobileA/http.js
@@ -0,0 +1,6 @@
+var http = require('http')
+http.createServer(function(request,response){
+  response.writeHead(200,{'Content-Type':'text/plain'});
+  response.end('Hello,world')
+}).listen(8088)
+console.log('成功1')
